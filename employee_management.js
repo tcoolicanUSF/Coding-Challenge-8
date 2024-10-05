135e16dce88679bfe3165c68fe5c2755d05f8dc5
tcoolican <tcoolican@usf.edu>
2024-10-05 00:06:26 -0400

    Create Department class to manage employees and calculate salary

--- employee_management.js
+++ employee_management.js
@@ -15,3 +15,25 @@ class Employee {
   console.log(employee1.getDetails());
   // Output: Daniel Ricciardo is a F1 Driver earning $1200000 per year.
   
+//Task 2: Create a Department Class
+class Department {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    getDepartmentSalary() {
+        return this.employees.reduce((total, employee) => total + employee.salary, 0);
+    }
+}
+// Example usage:
+const department1 = new Department("Aero Engineering");
+const employee2 = new Employee("Mark Webber", 1200000, "Driver");
+const employee3 = new Employee("Adrian Newey", 300000, "Aero Analyst");
+
+department1.addEmployee(employee2);
+department1.addEmployee(employee3);
+
+console.log(department1.getDepartmentSalary()); //Output: 1500000
