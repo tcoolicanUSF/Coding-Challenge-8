a152a1f0ea2a007fcffbb677a21ef82065491506
tcoolican <tcoolican@usf.edu>
2024-10-06 22:56:59 -0400

    Create and manage departments and employees

--- employee_management.js
+++ employee_management.js
@@ -17,11 +17,11 @@ class Employee {
   
 //Task 2: Create a Department Class
 class Department {
-    constructor(name) {
+    constructor(name, Driver) {
         this.name = name;
-        this.employees = [];
+        this.Driver = [];
     }
-    addEmployee(employee) {
+    addEmployee(Driver) {
         this.employees.push(employee);
     }
     getDepartmentSalary() {
@@ -37,3 +37,41 @@ department1.addEmployee(employee2);
 department1.addEmployee(employee3);
 
 console.log(department1.getDepartmentSalary()); //Output: 1500000
+
+//Task 3: Create a Manager Class that Inherits from Employee
+class Director extends Driver {
+    constructor (name, salary, position, team, bonus) {
+        super(name, salary, position, team)
+        this.bonus = bonus;
+    }
+}
+
+ //Task 4- Handle Bonuses for Managers
+ calculateTotalSalarywithBonus(); {
+    let totalSalaryNoBonus = this.getTotalSalary()
+
+    let totalBonusSalary =this.Driver
+        .filter(Driver => Driver.position === 'Director')
+        .reduce((bonusIndex, Director) => bonusIndex + Director.bonus, 0)
+
+    return totalSalaryNoBonus + totalBonusSalary
+}
+
+//Task 5 Create and manage departments and employees
+const RBR = new Team ('Red Bull Racing')
+const HAS = new Team ('HAAS Racing')
+
+const Mag = new Driver ('Magnussen', 140000, 'F1 Pilot', 'HAAS Racing')
+const Ver = new Driver ('Verstappen', 150000, 'F1 Pilot', 'Red Bull Racing')
+const Hor = new Director ('Horner', 200000, 'Director', 'Red Bull Racing', 30000)
+const Kom = new Director ('Komatsu', 220000, 'Director', 'HAAS Racing', 50000)
+
+RBR.addDriver(Ver)
+RBR.addDriver(Hor)
+HAS.addDriver(Mag)
+HAS.addDriver(Kom)
+
+console.log(`Total Salary for Red Bull Racing: ${RBR.getTeamSalary()}`);
+console.log(`Total Salary with coach bonus for Red Bull Racing: ${RBR.calculateTotalSalarywithBonus()}`)
+console.log(`Total Salary for HAAS Racing: ${HAS.getTeamSalary()}`)
+console.log(`Total Salary with coach bonus for HAAS Racing: ${HAS.calculateTotalSalarywithBonus()}`)
